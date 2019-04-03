3148386a6fba70561bd5c8f60c775aad9ffd5224
Ben <liftport@gmail.com>
2019-04-02 22:27:14 -0500

    Matrix is now being filled with integers.

--- Assign5/Form1.cs
+++ Assign5/Form1.cs
@@ -19,39 +19,76 @@ using System.IO;
 
 namespace Assign5 {
     public partial class Form1 : Form {
-        string filePath;
         string gameData;
+        int numColumns;
+
 
         public Form1() {
             InitializeComponent();
         }
 
         private void Form1_Load(object sender, EventArgs e) {
-            comboBoxDifficulty.Items.Add("Easy 1");
-            comboBoxDifficulty.Items.Add("Easy 2");
-            comboBoxDifficulty.Items.Add("Easy 3");
+            Dictionary<string, string> difficultyOptions = new Dictionary<string, string>();    // Holds game difficulty options for Difficulty dropdown menu.
 
+            // Add options to the Difficulty dropdown menu data source.
+            difficultyOptions.Add("easy/e1.txt", "Easy 1");
+            difficultyOptions.Add("easy/e2.txt", "Easy 2");
+            difficultyOptions.Add("easy/e3.txt", "Easy 3");
 
-            // Get file paths to each game.
-            using (var file = new StreamReader("../../../Resources/directory.txt")) {
-
-                // Read in each file path to each game.
-                while ((filePath = file.ReadLine()) != null) {
-                    // Open a single game's data file.
-                    using (var gameFilepath = new StreamReader("../../../Resources/" + filePath)) {
-                        // Read in game matrix data. Right now, I am reading it in as strings, when it should be a matrix of ints ultimately.
-                        while ((gameData = gameFilepath.ReadLine()) != null) {
-                            Console.WriteLine(gameData);
-                        }
-                        
-                        Console.WriteLine("\n");
-                    }
-                }
-            }
+            // Set the dropdown menu to use the data source.
+            comboBoxDifficulty.DataSource = new BindingSource(difficultyOptions, null);
+            comboBoxDifficulty.DisplayMember = "Value";
+            comboBoxDifficulty.ValueMember = "Key";
         }
 
         private void button1_Start(object sender, EventArgs e) {
-            Console.WriteLine(comboBoxDifficulty.SelectedIndex);
+            int row = 0, 
+                column = 0;
+
+
+            using (var gameFile = new StreamReader("../../../Resources/" + ((KeyValuePair<string, string>)comboBoxDifficulty.SelectedItem).Key)) {
+                gameData = gameFile.ReadLine();
+
+                // Read a row of numbers from the file and count how many numbers there are. That's how many columns and rows the game will have.
+                numColumns = (gameData.ToCharArray().Length);
+
+                // Get a character array of that row's numbers.
+                char[] charNums = gameData.ToCharArray();
+
+                // Create the two-dimensional array that will hold game numbers.
+                int[,] gameMatrix = new int[numColumns, numColumns];
+
+                // Fill in the first row of the matrix with numbers.
+                for (int i = 0; i < numColumns; i++) {
+                    gameMatrix[row, i] = (int)Char.GetNumericValue(charNums[i]);
+                }
+
+                // Move on to next row.
+                row++;
+
+                while ((gameData = gameFile.ReadLine()) != null) {
+                    // Get a character array of that row's numbers.
+                    charNums = gameData.ToCharArray();
+
+                    for (int i = 0; i < numColumns; i++) {
+                        gameMatrix[row, i] = (int) Char.GetNumericValue(charNums[i]);
+                    }
+
+                    // Move on to next row.
+                    row++;
+
+                    if (row == 3)
+                        break;
+                }
+
+                for (int r = 0; r < numColumns; r++) {
+                    for (int c = 0; c < numColumns; c++) {
+                        Console.Write(gameMatrix[r, c]);
+                    }
+                }
+
+                Console.WriteLine("\n");
+            }
         }
     }
 }
